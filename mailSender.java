69d9bd44da1746d23b488cd22018502110929bc4
horiyamayoh <horiyamayoh@gmail.com>
2018-11-03 17:48:20 +0900

    mailSenderクラスのやつに書き換えました

--- mailSender.java
+++ mailSender.java
@@ -9,21 +9,14 @@ import javax.mail.internet.MimeMessage;
 import com.sun.mail.smtp.SMTPTransport;
 
 public class mailSender {
-	public static void main(String args[]) {
-          String title,message,destination,youraddress,yourpassword;
-          title = "test title";
-          message = "test message";
-          destination = "room.seven.one.four@gmail.com";
-          youraddress = "room.seven.one.four@gmail.com";
-          yourpassword = "hukuzatsukei714room";
-          mailSend(title,message,destination,youraddress,yourpassword);
-	}
 
-	public static void mailSend(String title, String text, String toAddress, 
-		String fromAddress, String password) {
+	 mailSender(String fromAddress,
+                    String fromPassword,
+                    String toAddress, 
+                    String subject,
+                    String message) {
 
 		try {
-			// プロパティの設定
 			Properties props = System.getProperties();
 			props.put("mail.smtp.auth", "true");
 			props.put("mail.smtp.starttls.enable", "true");
@@ -33,26 +26,20 @@ public class mailSender {
 			Session session = Session.getInstance(props);
 			Message msg = new MimeMessage(session);
 
-			// 送信元メールアドレスのセット
 			msg.setFrom(new InternetAddress(fromAddress));
 
-			// 送信先メールアドレスのセット
 			msg.setRecipients(Message.RecipientType.TO, 
 				InternetAddress.parse(toAddress, false));
 
-			// メールタイトル
-			msg.setSubject(title);
+			msg.setSubject(subject);
 
-			// メール本文
-			msg.setText(text);
+			msg.setText(message);
 
-			// 送信日
 			msg.setSentDate(new Date());
 
-			// メール送信
 			SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
 			try {
-				t.connect("smtp.gmail.com", fromAddress, password);
+				t.connect("smtp.gmail.com", fromAddress, fromPassword);
 				t.sendMessage(msg, msg.getAllRecipients());
 			} finally {
 				t.close();
